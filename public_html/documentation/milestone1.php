4fcf4d10c63fccc408a15da0d94a6c406afa9171
Gus Liakos <Gliakos@cnm.edu>
2018-04-20 10:38:32 -0600

    update persona profession and outlook

--- public_html/documentation/milestone1.php
+++ public_html/documentation/milestone1.php
@@ -20,7 +20,8 @@
 	</p>
 	<h2>Goal:</h2>
 	<p>The goal of this project is primarily to be the platform for my online prescence, facilitating developing my portfolio as a writer, photographer, and software developer;
-		essentially an all-purpose brag-sheet with location/instance-specific sessions alongside a general about-me.</p>
+		essentially an all-purpose brag-sheet with location/instance-specific sessions alongside a general about-me.Every visitor should be "wow-ed" or at least appreciate the inherent
+		attention to detail.</p>
 		<!--The service is to have a kitsch-y theme in line with the "fly with me" motto and be known primarily as an image-development organization. </p>-->
 </section>
 <section id="Persona">
@@ -31,28 +32,30 @@
 	<div id="student">
 		<h2>Profession:</h2>
 		<p>Allison is a grad student in her first year at an middle-America based masters program, and although she is engaged as a Great-depression specialized historian,
-			her hobby/passion of human-interest journalism has been steadily growing and she is looking to start her own online news agency. To this extent she is looking for contibrutors and individuals to grow her platform.</p>
+			her hobby/passion of human-interest journalism has been steadily growing and she is looking to start her own online news agency. To this extent she is looking for
+			contributors and individuals to grow her platform.</p>
 	</div><div id="tech">
 		<h2>Technology:</h2>
 		<p>Iphone 8 on IOS 10.3.3</p>
-		<p>macbook</p>
+		<p>*macbook</p>
 	</div><div id="behaviors">
 		<h2>Outlook</h2>
 		<p>Allison is simultaneously excited and apprehensive about her future. She is engaged socially and on social media, yet nonetheless does not feel
-			like her image or "online story" is crafted well for when she goes to university. She is comfortable paying a premium price for excellent photos.</p>
+			like her image or "online story" is powerful or unique enough to reach the influencer status she seeks. She is comfortable paying a premium price
+			for excellent work to execute her vision. She is discerning and quick witted. adept with technology yet not a software developer.</p>
 	</div><div id="needs">
 		<h2>Frustrations</h2>
-		<ul>
-			<li>Other photographers are often difficult or require extended wait periods to schedule photo sessions.</li>
-			<li>Professional portrait photographers can often be comfortable in there studio or select locales, many the same as used by Allison's peers</li>
-			<li>Each independent photographer has there own style and their own rates, and there is no easy way to shop around.</li>
-		</ul>
+<!--		<ul>-->
+<!--			<li>Other photographers are often difficult or require extended wait periods to schedule photo sessions.</li>-->
+<!--			<li>Professional portrait photographers can often be comfortable in there studio or select locales, many the same as used by Allison's peers</li>-->
+<!--			<li>Each independent photographer has there own style and their own rates, and there is no easy way to shop around.</li>-->
+<!--		</ul>-->
 	</div><div id="Goals">
 		<h2>Goals</h2>
-		<p>To browse a selection of photographers and/or locations and be able to schedule a safe, unique, hiqh quality photoshoot for her senior photos.
+		<p>To browse a selection of photographers and/or writers and be able to schedule a safe, unique, hiqh quality photoshoot for her senior photos.
 		</p>
 	</div><div id="user-story">
 		<h2>User Story</h2>
-		<p>As a user, I need to browse the providers and select my custom, one-of-a-kind photo experience.<em>"To Fly Away"</em></p>
+		<p>As a user, I need to browse content providers and create my custom, one-of-a-kind team.</p>
 	</div>
 </section>
