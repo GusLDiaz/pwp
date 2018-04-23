4a9a61cfd86c904965a426d3300cfcfa051660c1
Gus Liakos <Gliakos@cnm.edu>
2018-04-23 11:52:20 -0600

    format html

--- public_html/documentation/milestone1.php
+++ public_html/documentation/milestone1.php
@@ -4,53 +4,72 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
-<section id="title">Glad Lad</section>
-<section id="purpose-audience-goal">
-	<h1>Persona, Audience, Goal</h1>
-	<h2>Purpose:</h2>
-	<p>This project will be a professional portfolio and blogspace to demonstrate ability to create and display "photostories",featuring four primary attributes: design, image, writing, and insight.</p>
-	<h2>Audience:</h2>
-	<p>Online (freelancing) clients, journalists, authors, potential employers, and casual protocultural enthusiasts : with an amalgamation of top-quality work, attract a range of individuals
-		interested in collaboration, helping start my next adventure, or pointed me in the right direction.</p>
-	<h2>Goal:</h2>
-	<p>The goal of this project is primarily to be the platform for my online prescence, facilitating developing my portfolio as a writer, photographer, and software developer;
-		essentially an all-purpose brag-sheet with location/instance-specific sessions alongside a general about-me.Every visitor should be "wow-ed" or at least appreciate the inherent
-		attention to detail.</p>
-</section>
-<section id="Persona">
-	<h1>Persona</h1>
-	<h2>Name: <strong>Allison Wonders</strong></h2>
-	<h2>Age: 22</h2>
-	<h2>Gender: Female</h2>
-	<div id="student">
-		<h2>Profession:</h2>
-		<p>Allison is a grad student in her first year at an middle-America based masters program, and although she is engaged as a Great-depression specialized historian,
-			her hobby/passion of human-interest journalism has been steadily growing and she is looking to start her own online news platform,<em>Don'tWait.io</em>. To this extent she is looking for
-			contributors and individuals to grow her platform.</p>
-	</div><div id="tech">
-		<h2>Technology:</h2>
-		<p>Iphone 8 on IOS 10.3.3</p>
-		<p>*macbook</p>
-	</div><div id="behaviors">
-		<h2>Outlook</h2>
-		<p>Allison is simultaneously excited and apprehensive about her future. She is engaged socially and on social media, including pinterest, LinkedIn, Instagram, and Twitter.
-			yet nonetheless does not feel like her image or "online story" is powerful or unique enough through social media to reach the <em>influencer</em> status she seeks.
-			She is comfortable paying a premium price for excellent work to execute her vision. She is discerning and quick witted. adept with technology yet not a software developer.</p>
-	</div><div id="needs">
-		<h2>Frustrations</h2>
-		<p>Online collaboration can be intimidating. Working across a time-space divide often inhibits efficiency and can slow down or prevent the execution of <em>DontWait.io</em>.
-		After interacting with several sub-par collaborators, who don't communicate effectively, or stick to deadlines, or are stubborn to implement feedback, Allison understands the importance
-		of hiring individuals with a professional mindset.</p>
-	</div><div id="Goals">
-		<h2>Goals</h2>
-		<p>To browse a selection of photographers and/or writers and be able to find a hiqh quality freelancer to incorporate into her organization at least through freelance work.
-			It's the responsibility of this freelancer to breed confidence in their qualifications and demonstrate they can overcome the obstacles of working remotely, and generate
-			consistent, high quality content on at least a weekly basis. Even better, she hopes to find a producer that could lead a team and join the org as a manager to expedite its growth.
-		</p>
-	</div><div id="user-story">
-		<h2>User Story</h2>
-		<p>As a user, I need to browse content providers and create my custom, one-of-a-kind team.</p>
-	</div>
-</section>
+		<section id="title">Glad Lad</section>
+		<section id="purpose-audience-goal">
+			<h1>Persona, Audience, Goal</h1>
+			<h2>Purpose:</h2>
+			<p>This project will be a professional portfolio and blogspace to demonstrate ability to create and display
+				"photostories",featuring four primary attributes: design, image, writing, and insight.</p>
+			<h2>Audience:</h2>
+			<p>Online (freelancing) clients, journalists, authors, potential employers, and casual protocultural
+				enthusiasts : with an amalgamation of top-quality work, attract a range of individuals
+				interested in collaboration, helping start my next adventure, or pointed me in the right direction.</p>
+			<h2>Goal:</h2>
+			<p>The goal of this project is primarily to be the platform for my online prescence, facilitating developing my
+				portfolio as a writer, photographer, and software developer;
+				essentially an all-purpose brag-sheet with location/instance-specific sessions alongside a general
+				about-me.Every visitor should be "wow-ed" or at least appreciate the inherent
+				attention to detail.</p>
+		</section>
+		<section id="Persona">
+			<h1>Persona</h1>
+			<h2>Name: <strong>Allison Wonders</strong></h2>
+			<h2>Age: 22</h2>
+			<h2>Gender: Female</h2>
+			<div id="student">
+				<h2>Profession:</h2>
+				<p>Allison is a grad student in her first year at an middle-America based masters program, and although she
+					is engaged as a Great-depression specialized historian,
+					her hobby/passion of human-interest journalism has been steadily growing and she is looking to start her
+					own online news platform,<em>Don'tWait.io</em>. To this extent she is looking for
+					contributors and individuals to grow her platform.</p>
+			</div>
+			<div id="tech">
+				<h2>Technology:</h2>
+				<p>Iphone 8 on IOS 10.3.3</p>
+				<p>*macbook</p>
+			</div>
+			<div id="behaviors">
+				<h2>Outlook</h2>
+				<p>Allison is simultaneously excited and apprehensive about her future. She is engaged socially and on
+					social media, including pinterest, LinkedIn, Instagram, and Twitter.
+					yet nonetheless does not feel like her image or "online story" is powerful or unique enough through
+					social media to reach the <em>influencer</em> status she seeks.
+					She is comfortable paying a premium price for excellent work to execute her vision. She is discerning and
+					quick witted. adept with technology yet not a software developer.</p>
+			</div>
+			<div id="needs">
+				<h2>Frustrations</h2>
+				<p>Online collaboration can be intimidating. Working across a time-space divide often inhibits efficiency
+					and can slow down or prevent the execution of <em>DontWait.io</em>.
+					After interacting with several sub-par collaborators, who don't communicate effectively, or stick to
+					deadlines, or are stubborn to implement feedback, Allison understands the importance
+					of hiring individuals with a professional mindset.</p>
+			</div>
+			<div id="Goals">
+				<h2>Goals</h2>
+				<p>To browse a selection of photographers and/or writers and be able to find a hiqh quality freelancer to
+					incorporate into her organization at least through freelance work.
+					It's the responsibility of this freelancer to breed confidence in their qualifications and demonstrate
+					they can overcome the obstacles of working remotely, and generate
+					consistent, high quality content on at least a weekly basis. Even better, she hopes to find a producer
+					that could lead a team and join the org as a manager to expedite its growth.
+				</p>
+			</div>
+			<div id="user-story">
+				<h2>User Story</h2>
+				<p>As a user, I need to browse content providers and create my custom, one-of-a-kind team.</p>
+			</div>
+		</section>
 	</body>
 </html>
