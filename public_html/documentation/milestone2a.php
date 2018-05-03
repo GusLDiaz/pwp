1e299f8207ecd3c9db3a070d429245645b1e469f
Gus Liakos <Gliakos@cnm.edu>
2018-05-03 12:43:29 -0600

    Added Content Strategy

---
+++ public_html/documentation/milestone2a.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gusli
+ * Date: 5/3/2018
+ * Time: 9:19 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2 Alpha</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>My site will be one page viewed on desktop, and several "pages" (cycleable styled as a carosel) composed of
+			these primary components:
+		<p>
+		<ol>
+			<li><strong>Splash:</strong> The landing page-view (for mobile) and for desktop top/first seen section will be
+				a brief one-line introduction will clearly state my name and visions for career advancement as a Front End
+				Developer,
+				accompanying a photo, all of which will be superimposed on a large background image of a desert
+				texturescape.
+			</li>
+			<li><strong>PhotoJournals:<strong> I want this to be a responsive Javascript-enabled page component featuring
+						4-7 embedded images each associated with an short article.The sidebar will cleanly display the
+						inactive images and in the center will be the active image larger and in a simple frame, with the
+						article text displayed alongside.clicking one of the inactive buttons or arrows nearby will cycle the
+						next image to the forefront and display its associated text.
+			</li>
+			<li><strong>Skills</strong>: This section will display my skill-set in punchy, simple labels around a central
+				image. I will have the bottom third of the mobile screen and side panel of the horizontal section on desktop
+				as a simple contact form.The content will
+				be shown in a sleek and professional tone as I am looking to portray myself as an informed,
+				innovation-minded developer.
+			</li>
+		</ol>
+		<p>Finally, all of these page- views will feature a small footer with functional iconography representing my
+			social media links: Github, LinkedIn, and Instagram.</p>
+
+		<h2>Wireframe Images</h2>
+	</body>
+</html>
