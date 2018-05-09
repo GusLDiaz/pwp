6f67a1bcf561126b7dc2c21088a438bcb63ccca8
Gus Liakos <Gliakos@cnm.edu>
2018-05-09 00:30:24 -0600

    fixed grammatical errors

--- public_html/documentation/milestone2a.php
+++ public_html/documentation/milestone2a.php
@@ -19,25 +19,36 @@
 		<p>
 		<ol>
 			<li><strong>Splash:</strong> The landing page-view and first seen section will consist of my portrait,
-				name, and a list display with punchy titles accompanied by icons hyperlinked to appropriate media: I.E. "Fullstack Developer" accompanied by a github icon.
-				Also in this section will be a button labeled PhotoJournals that presents a Bootstrap Modal describing the concept of the next section
-				and offering instructions to the end user. Next to the button are the thumbnails of each of my current hosted PhotoJournals.
+				name, and a list display with punchy titles accompanied by icons hyperlinked to appropriate media: I.E.
+				"Fullstack Developer" accompanied by a github icon.
+				Also in this section will be a button labeled PhotoJournals that presents a Bootstrap Modal describing the
+				concept of the next section
+				and offering instructions to the end user. Next to the button are the thumbnails of each of my current
+				hosted PhotoJournals.
 			</li>
-			<li><strong>PhotoJournals:</strong> I want this to be a responsive Javascript-enabled page component that will host 4-7 "PhotoJournals".
-				The section will cleanly display one of my PhotoJournals: a single image in a rounded frame and similar to a Bootstrap card with a short title and
-				a piece of flash fiction relating to the photo, which I describe as the "Journal". Beneath and/or beside the image will
-				be a minimalist pager that allows the user to click previous or next, and functionally go to the next PhotoJournal. As needed this section may have a
-				separate container background overlaid on the general site for ease of viewing.
+			<li><strong>PhotoJournals:</strong> I want this to be a responsive Javascript-enabled page component that will
+				host 4-7 "PhotoJournals".
+				The section will cleanly display one of my PhotoJournals: a single image in a rounded frame and similar to a
+				Bootstrap card with a short title and a piece of flash fiction relating to the photo, which I describe as
+				the "Journal". Beneath and/or beside the
+				image will be a minimalist pager that allows the user to click previous or next, and functionally go to the
+				next
+				PhotoJournal. As needed this section may have a separate container background overlaid on the general site
+				for ease of viewing.
 			</li>
-			<li><strong>Contact Me:</strong>: This section will display a punchy, simple contact form. Their will be fields
-				for input of name, email, and the actual message with corresponding iconography. this will be floating and isolated
-				in the center of the section for a no-nonsense, professional tone.
+			<li><strong>Contact Me:</strong>: This section will display a punchy, simple contact form. There will be fields
+				for the user to input their name, email, and the actual message, with corresponding iconography.
+				This will be isolated in the center of the section for a no-nonsense, professional tone.
 			</li>
 		</ol>
-		<p>These sections will be separated by minimalist dividers that span across the screen.</p>
+		<p>These sections will be separated by minimalist dividers that span across the screen. </p>
 
 		<h2>Wireframe Images</h2>
-		<a href="pwp_mobile.png">Mobile</a>
-		<a href="pwp_desktop.png">Desktop</a>
+		<ul>
+			<li>
+				<a href="pwp_mobile.png">Mobile</a></li>
+			<li>
+				<a href="pwp_desktop.png">Desktop</a></li>
+		</ul>
 	</body>
 </html>
