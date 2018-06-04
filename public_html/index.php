30a5c6cdbf673c349f4efba20ea2b2193be1d868
Gus Liakos <Gliakos@cnm.edu>
2018-06-03 21:04:58 -0600

    Add boiler plate for Slick carousel add

--- public_html/index.php
+++ public_html/index.php
@@ -3,10 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-		<link rel="stylesheet" href="desktop.css">
-		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
 				  integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
@@ -15,12 +12,19 @@
 				  integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
 				  crossorigin="anonymous"></script>
 		<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-		<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+		<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.0/slick/slick.min.js"></script>
+		<script type="text/javascript" rel="script" src="slicks.js"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="desktop.css">
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+
 		<title>GladLad</title>
 	</head>
 	<body class="whole">
 		<section>
-			<div class="container-fluid bgimg" >
+			<div class="container-fluid bgimg">
 				<div class="row-fluid">
 					<div class="column">
 						<div class="container pt-5">
@@ -33,90 +37,85 @@
 		</section>
 		<section>
 			<div class="container about d-flex flex-sm-wrap mw-100 ">
-			<img src="images/lionteeth.jpg" alt="ricky" class="img-fluid rounded-circle col-sm-12 col-md-6 row-sm-12">
+				<img src="images/lionteeth.jpg" alt="ricky" class="img-fluid rounded-circle col-sm-12 col-md-6 row-sm-12">
 				<div class="list-group flex-grow align-self-end rounded b border-dark">
 					<a class="list-group-item-dark pb-3 display-2 pr-2 ">
-					Developer
+						Developer
 					</a>
 					<a class="list-group-item-dark pb-3 pr-2 display-4 justify-content-between align-items-center">
-					Photographer
+						Photographer
 					</a>
 					<a class="list-group-item-dark display-4 pb-3 justify-content-between align-items-center">
-					Writer
+						Writer
 						<span class="badge badge-primary badge-pill lead badge-dark" href="#!">Medium</span>
 					</a>
 				</div>
 			</div>
 		</section>
 		<div class="container">
-			<div class="d-flex ml-md-4 ml-sm-1 redbar"></div>
+			<div class="ml-md-4 ml-sm-1 redbar"></div>
 		</div>
 		<section>
-			<div class="d-flex ml-md-5 ml-sm-1">
-<!--				<div class="d-inline-block">-->
-<!--					<img class="img-fluid rounded flex-shrink-2" src="images/man-1351317.svg" alt="Card image cap">-->
-<!--					<div class="ml-5 align-content-center">-->
-<!--<!--						<div class="btn-group btn-group-lg " aria-label="Gus">-->
-<!--<!--							<button type="button" class="btn btn-secondary mr-3 ml-5" id="left"><</button>-->
-<!--<!--							<button type="button" class="btn btn-secondary ml-3" id="right">></button>-->
-<!--<!--						</div><div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">-->
-<!--							<div class="btn-group" role="group" aria-label="First group">-->
-<!--								<button type="button" class="btn btn-secondary">1</button>-->
-<!--								<button type="button" class="btn btn-secondary">2</button>-->
-<!--								<button type="button" class="btn btn-secondary">3</button>-->
-<!--							</div>-->
-<!--							<link rel="shortcut icon" href="favico" type="image/x-icon">-->
-<!--							<div class="btn-group" role="group" aria-label="Third group">-->
-<!--								<button type="button" class="btn btn-secondary">more</button>-->
-<!--							</div>-->
+			<div class="container-fluid d-block">
+<!--				<div id="carouselStories" class="carousel slide carousel-fade" data-ride="carousel">-->
+<!--					<div class="carousel-inner" role="listbox">-->
+<!--						<div class="carousel-item active">-->
+<!--							<img class="d-block w-100" src="#" data-src="./images/flower.jpg" alt="First slide">-->
+<!--						</div>-->
+<!--						<div class="carousel-item">-->
+<!--							<img class="d-block w-100" src="#" data-src="./images/test-portrait.jpg" alt="Second slide">-->
 <!--						</div>-->
 <!--					</div>-->
+<!--					<a class="carousel-control-prev" href="#carouselStories" role="button" data-slide="prev">-->
+<!--						<span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--						<span class="sr-only">Previous</span>-->
+<!--					</a>-->
+<!--					<a class="carousel-control-next" href="#carouselStories" role="button" data-slide="next">-->
+<!--						<span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--						<span class="sr-only">Next</span>-->
+<!--					</a>-->
 <!--				</div>-->
-				<div class="card col-md-3 pt-2">
-					<img class="card-img-top" src="images/flower.jpg" alt="Card image cap">
-					<div class="card-body align-items-center bg-dark">
-						<div class="btn-toolbar  d-flex">
-														<div class="btn-group" role="group" >
-															<button type="button" class="btn btn-secondary">1</button>
-															<button type="button" class="btn btn-secondary">2</button>
-															<button type="button" class="btn btn-secondary">3</button>
-															<button type="button" class="btn btn-secondary">more</button>
 
-														</div>
-<!--						<a href="#!" class="btn btn-primary">Go somewhere</a>-->
+
+				<!--				<div class="card col-md-3 pt-2">-->
+				<div class="row">
+					<div class="single-item pt-2">
+						<div><img src="images/flower.jpg" alt="Card image cap"></div>
+						<div><img src="images/test-portrait.jpg"></div>
+						<div><img src="images/test-honey.jpg"></div>
 					</div>
 				</div>
-				</div>
 
+			</div>
 
-				<!--				<--java script hiding ability?-->
-				<div class="card-body d-flex flex-column col-md-9">
-					<h4 class="card-title order-1 d-block">ALABAMA SUNSHINE</h4>
-					<h6 class="card-subtitle mb-2 text-muted d-block order-2">When Two Was Too Many</h6>
-					<p class="card-text order-3 p-1" id="tbox">
-						and make up the bulk of the card's content.Tumeric pop-up lumbersexual, aliquip ea four dollar
-						toast
-						deep v palo santo XOXO air plant seitan green juice unicorn. YOLO magna sed nostrud craft beer,
-						+1
-						vinyl photo booth sriracha four dollar toast butcher. Tattooed taxidermy authentic, trust fund
-						forage
-						dreamcatcher narwhal lyft cloud bread kombucha lomo wolf pork belly. Schlitz hell of wolf
-						Letterpress marfa kinfolk raclette. Adipisicing fam quis, exercitation irure fixie food truck
-						vegan
-						cronut lyft ullamco. Listicle iPhone occaecat jianbing chia. Meh bespoke leggings post-ironic.
-						Chartreuse ullamco deep v, kombucha hoodie gochujang ugh listicle beard subway tile pop-up
-						microdosing
-						typewriter enim. You probably haven't heard of them hashtag fanny pack,
-						mumblecore church-key williamsburg incididunt pug biodiesel. Williamsburg green juice dolore
-						enamel
-						pin nisi artisan magna est cray proident poutine truffaut.
-						+1 vaporware
-						I bet you’re still using Bootstrap too…
-						The Lab Bunny — One time beauty school suspendee, two time college dropout, full time lipstick
-						hoarder. Libra. Non-smoker. DTF
-					</p>
-					<a href="#!" class="btn btn-success order-8">full story</a>
-				</div>
+			<div class="card-body d-flex flex-column col-md-9">
+				<h4 class="card-title order-1 d-block" id="print">ALABAMA SUNSHINE</h4>
+				<h6 class="card-subtitle mb-2 text-muted d-block order-2">When Two Was Too Many</h6>
+				<p class="card-text order-3 p-1" id="tbox">
+					and make up the bulk of the card's content.Tumeric pop-up lumbersexual, aliquip ea four dollar
+					toast
+					deep v palo santo XOXO air plant seitan green juice unicorn. YOLO magna sed nostrud craft beer,
+					+1
+					vinyl photo booth sriracha four dollar toast butcher. Tattooed taxidermy authentic, trust fund
+					forage
+					dreamcatcher narwhal lyft cloud bread kombucha lomo wolf pork belly. Schlitz hell of wolf
+					Letterpress marfa kinfolk raclette. Adipisicing fam quis, exercitation irure fixie food truck
+					vegan
+					cronut lyft ullamco. Listicle iPhone occaecat jianbing chia. Meh bespoke leggings post-ironic.
+					Chartreuse ullamco deep v, kombucha hoodie gochujang ugh listicle beard subway tile pop-up
+					microdosing
+					typewriter enim. You probably haven't heard of them hashtag fanny pack,
+					mumblecore church-key williamsburg incididunt pug biodiesel. Williamsburg green juice dolore
+					enamel
+					pin nisi artisan magna est cray proident poutine truffaut.
+					+1 vaporware
+					I bet you’re still using Bootstrap too…
+					The Lab Bunny — One time beauty school suspendee, two time college dropout, full time lipstick
+					hoarder. Libra. Non-smoker. DTF
+				</p>
+				<a href="#!" class="btn btn-success order-8">full story</a>
+			</div>
+
 			</div>
 
 		</section>
@@ -124,15 +123,8 @@
 			<div class="container"></div>
 		</section>
 		<div class="container">
-			<div class="d-flex ml-5 redbar"></div>
+			<div class=" ml-5 redbar"></div>
 		</div>
-<!--		<section id=" contact">-->
-<!--			<div class="container">-->
-<!--				<div>-->
-<!--					<form action=""></form>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</section>-->
 		<section>
 			<div class="container-fluid frac" id="contact-section">
 				<div class="container">
@@ -145,7 +137,8 @@
 									<div class="input-group border">
 										<div class="input-group-addon">
 										</div>
-										<input type="text" class="form-control" id="contactFormName" name="contactFormName" placeholder="Your name">
+										<input type="text" class="form-control" id="contactFormName" name="contactFormName"
+												 placeholder="Your name">
 									</div>
 								</div>
 								<div class="form-group">
@@ -153,7 +146,8 @@
 									<div class="input-group border">
 										<div class="input-group-addon">
 										</div>
-										<input type="email" class="form-control" id="contactFormEmail" name="contactFormEmail" placeholder="Your email address">
+										<input type="email" class="form-control" id="contactFormEmail" name="contactFormEmail"
+												 placeholder="Your email address">
 									</div>
 								</div>
 								<div class="form-group">
@@ -161,7 +155,8 @@
 									<div class="input-group border">
 										<div class="input-group-addon">
 										</div>
-										<input type="text" class="form-control" id="contactFormSubject" name="contactFormSubject" placeholder="Email subject line">
+										<input type="text" class="form-control" id="contactFormSubject" name="contactFormSubject"
+												 placeholder="Email subject line">
 									</div>
 								</div>
 								<div class="form-group">
@@ -169,10 +164,15 @@
 									<div class="input-group border">
 										<div class="input-group-addon">
 										</div>
-										<textarea class="form-control" rows="5" id="contactFormMessage" name="contactFormMessage" placeholder="Message for Me":></textarea>
+										<textarea class="form-control" rows="5" id="contactFormMessage" name="contactFormMessage"
+													 placeholder="Message for Me" :></textarea>
 									</div>
 								</div>
 
-								</div>
+						</div>
+
+						<script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+
+
 	</body>
 </html>
---
+++ public_html/slicks.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".single-item").slick({
+	dots: false,
+	infinite: false,
+	speed: 500,
+	fade: true,
+	adaptiveHeight: true,
+});
+});
+
