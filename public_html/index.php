729f32c30e16c5a6df55ffa9e705d8256b176481
Gus Liakos <Gliakos@cnm.edu>
2018-06-04 14:18:03 -0600

    clean up head html

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,8 @@
 		<!--		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>-->
 		<!--		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
 		<!--		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>-->
-		<!--		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>-->
+
+		<!-- Bootstrap Js/Dependencies: Jquery , Popper, bootstrap -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
 				  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
 				  crossorigin="anonymous"></script>
@@ -16,15 +17,19 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
 				  integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
 				  crossorigin="anonymous"></script>
+
+		<!-- Slick: -->
 		<!--		<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>-->
 		<!--		<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.0/slick/slick.min.js"></script>-->
 		<!--		<script type="text/javascript" rel="script" src="slicks.js"></script>-->
+
+		<!--Css: bootstrap and local -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 		<link rel="stylesheet" href="desktop.css">
+
+		<!--Favicon setup -->
 		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 		<link rel="icon" href="images/favicon.ico" type="image/x-icon">
-
-
 		<title>GladLad</title>
 	</head>
 	<body class="whole">
@@ -41,88 +46,103 @@
 			</div>
 		</section>
 		<section>
-			<div class="container-fluid about d-flex flex-sm-wrap mw-100 mh-100 ">
+			<div class="container-fluid about align-content-around d-flex flex-sm-wrap mw-100 mh-100 ">
 				<img src="images/lionteeth.jpg" alt="ricky" class="rounded-circle col-xs-12 col-sm-12 col-md-6 row-sm-12">
-				<div class="list-group flex-grow align-self-end rounded b border-dark">
-					<a class="list-group-item-dark pb-3 display-2 pr-2 ">
-						Developer
+				<!--				<div class="list-group align-self-end rounded b border-dark">-->
+				<!--					<a class="list-group-item-dark pb-3 display-2 pr-2 ">-->
+				<!--						Developer-->
+				<!--					</a>-->
+				<!--					<a class="list-group-item-dark pb-3 pr-2 display-4 justify-content-between align-items-center">-->
+				<!--						Photographer-->
+				<!--					</a>-->
+				<!--					<a class="list-group-item-dark display-4 pb-3 justify-content-between align-items-center">-->
+				<!--						Writer-->
+				<!--								<span class="badge badge-primary badge-pill lead badge-dark" href="#!">Medium</span>-->
+				<!--					</a>-->
+				<div class="align-baseline col-md-3">
+					<div class="btn-group float-right">
+						<button type="button" class="btn btn-secondary display-1 dropdown-toggle" data-toggle="dropdown"
+								  aria-haspopup="true" aria-expanded="false">
+							Find Me on:
+						</button>
+						<div class="dropdown-menu dropdown-menu-right">
+							<button class="dropdown-item" type="button">Github</button>
+							<button class="dropdown-item" type="button">LinkedIn</button>
+							<button class="dropdown-item" type="button">Medium</button>
+						</div>
+					</div>
+					<!--						<button type="button" class="btn btn-secondary">Example Button floated right</button>-->
+				</div>
+
+		</div>
+	</div>
+</section>
+<div class="container">
+	<div class="ml-md-4 ml-sm-1 redbar"></div>
+</div>
+<section>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+				<div id="carouselStories" class="carousel slide carousel-fade" data-ride="carousel">
+					<div class="carousel-inner" role="listbox">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="images/flower.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="images/test-portrait.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="images/test-honey.jpg" alt="Third slide">
+						</div>
+					</div>
+					<a class="carousel-control-prev" href="#carouselStories" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
 					</a>
-					<a class="list-group-item-dark pb-3 pr-2 display-4 justify-content-between align-items-center">
-						Photographer
+					<a class="carousel-control-next" href="#carouselStories" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
 					</a>
-					<a class="list-group-item-dark display-4 pb-3 justify-content-between align-items-center">
-						Writer
-						<!--						<span class="badge badge-primary badge-pill lead badge-dark" href="#!">Medium</span>-->
-					</a>
+				</div>
+				<!--				<div class="row">-->
+				<!--					<div class="single-item pt-2">-->
+				<!--						<div><img src="images/flower.jpg" alt="Card image cap"></div>-->
+				<!--						<div><img src="images/test-portrait.jpg"></div>-->
+				<!--						<div><img src="images/test-honey.jpg"></div>-->
+				<!--					</div>-->
+				<!--				</div>-->
+			</div>
+			<div class="card col-sm-12 col-md-8 bg-dark">
+				<div class="card-body d-flex flex-column">
+					<h4 class="card-title order-1 d-block" id="print">ALABAMA SUNSHINE</h4>
+					<h6 class="card-subtitle mb-2 text-muted d-block order-2">When Two Was Too Many</h6>
+					<p class="card-text order-3 p-1" id="tbox">
+						and make up the bulk of the card's content.Tumeric pop-up lumbersexual, aliquip ea four dollar
+						toast
+						deep v palo santo XOXO air plant seitan green juice unicorn. YOLO magna sed nostrud craft beer,
+						+1
+						vinyl photo booth sriracha four dollar toast butcher. Tattooed taxidermy authentic, trust fund
+						forage
+						dreamcatcher narwhal lyft cloud bread kombucha lomo wolf pork belly. Schlitz hell of wolf
+						Letterpress marfa kinfolk raclette. Adipisicing fam quis, exercitation irure fixie food truck
+						vegan
+						cronut lyft ullamco. Listicle iPhone occaecat jianbing chia. Meh bespoke leggings post-ironic.
+						Chartreuse ullamco deep v, kombucha hoodie gochujang ugh listicle beard subway tile pop-up
+						microdosing
+						typewriter enim. You probably haven't heard of them hashtag fanny pack,
+						mumblecore church-key williamsburg incididunt pug biodiesel. Williamsburg green juice dolore
+						enamel
+						pin nisi artisan magna est cray proident poutine truffaut.
+						+1 vaporware
+						I bet you’re still using Bootstrap too…
+						The Lab Bunny — One time beauty school suspendee, two time college dropout, full time lipstick
+						hoarder. Libra. Non-smoker. DTF
+					</p>
+					<a href="#!" class="btn btn-success order-8">full story</a>
 				</div>
 			</div>
-		</section>
-		<div class="container">
-			<div class="ml-md-4 ml-sm-1 redbar"></div>
-		</div>
-		<section>
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-4">
-						<div id="carouselStories" class="carousel slide carousel-fade" data-ride="carousel">
-							<div class="carousel-inner" role="listbox">
-								<div class="carousel-item active">
-									<img class="d-block w-100" src="images/flower.jpg" alt="First slide">
-								</div>
-								<div class="carousel-item">
-									<img class="d-block w-100" src="images/test-portrait.jpg" alt="Second slide">
-								</div>
-								<div class="carousel-item">
-									<img class="d-block w-100" src="images/test-honey.jpg" alt="Third slide">
-								</div>
-							</div>
-							<a class="carousel-control-prev" href="#carouselStories" role="button" data-slide="prev">
-								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-								<span class="sr-only">Previous</span>
-							</a>
-							<a class="carousel-control-next" href="#carouselStories" role="button" data-slide="next">
-								<span class="carousel-control-next-icon" aria-hidden="true"></span>
-								<span class="sr-only">Next</span>
-							</a>
-						</div>
-						<!--				<div class="row">-->
-						<!--					<div class="single-item pt-2">-->
-						<!--						<div><img src="images/flower.jpg" alt="Card image cap"></div>-->
-						<!--						<div><img src="images/test-portrait.jpg"></div>-->
-						<!--						<div><img src="images/test-honey.jpg"></div>-->
-						<!--					</div>-->
-						<!--				</div>-->
-					</div>
-					<div class="card col-md-8">
-						<div class="card-body d-flex flex-column">
-							<h4 class="card-title order-1 d-block" id="print">ALABAMA SUNSHINE</h4>
-							<h6 class="card-subtitle mb-2 text-muted d-block order-2">When Two Was Too Many</h6>
-							<p class="card-text order-3 p-1" id="tbox">
-								and make up the bulk of the card's content.Tumeric pop-up lumbersexual, aliquip ea four dollar
-								toast
-								deep v palo santo XOXO air plant seitan green juice unicorn. YOLO magna sed nostrud craft beer,
-								+1
-								vinyl photo booth sriracha four dollar toast butcher. Tattooed taxidermy authentic, trust fund
-								forage
-								dreamcatcher narwhal lyft cloud bread kombucha lomo wolf pork belly. Schlitz hell of wolf
-								Letterpress marfa kinfolk raclette. Adipisicing fam quis, exercitation irure fixie food truck
-								vegan
-								cronut lyft ullamco. Listicle iPhone occaecat jianbing chia. Meh bespoke leggings post-ironic.
-								Chartreuse ullamco deep v, kombucha hoodie gochujang ugh listicle beard subway tile pop-up
-								microdosing
-								typewriter enim. You probably haven't heard of them hashtag fanny pack,
-								mumblecore church-key williamsburg incididunt pug biodiesel. Williamsburg green juice dolore
-								enamel
-								pin nisi artisan magna est cray proident poutine truffaut.
-								+1 vaporware
-								I bet you’re still using Bootstrap too…
-								The Lab Bunny — One time beauty school suspendee, two time college dropout, full time lipstick
-								hoarder. Libra. Non-smoker. DTF
-							</p>
-							<a href="#!" class="btn btn-success order-8">full story</a>
-						</div>
-					</div>
-				</div>
+			</div>
 		</section>
 		<section>
 			<div class="container"></div>
